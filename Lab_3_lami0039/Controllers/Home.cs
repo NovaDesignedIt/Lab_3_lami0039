bda221df7ea1faf6bc02d9a6695c87cb2295969e
mch <mch@DESKTOP-MQDUTH4>
2021-10-12 14:30:23 -0700

    Add project files.

---
+++ Lab_3_lami0039/Controllers/Home.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+   
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+            
+        }
+
+        public IActionResult Razor() => View("~/Views/Home/Razor.cshtml");
+
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+           
+             ViewData["number"] = int.Parse(Request.Form["number"]);
+            return View("~/Views/Home/Count.cshtml","~/Views/Shared/Layout.cshtml");
+
+            
+        }
+
+
+
+        public IActionResult CreatePerson() => View("~/Views/Home/CreatePerson.cshtml","~/Views/shared/Layout.cshtml");
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Models.Person person)
+        {
+            person.Age = Request.Form["age"];
+            person.First_name = Request.Form["fname"];
+            person.description = Request.Form["description"];
+            person.Last_name = Request.Form["lname"];
+            person.password = Request.Form["lname"];
+            person.Date_of_birth = Request.Form["dob"];
+            person.EmailAddress = Request.Form["email"];
+
+            return View(person);
+
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Lab_3_lami0039/Models/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Lab3.Models
+{
+    public class Person
+
+    {
+      
+        [Required]
+        [StringLength(255)]
+        [BindProperty]
+        public string First_name
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [BindProperty]
+        public string Last_name
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [BindProperty]
+        public string Age
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [BindProperty]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+        [BindProperty]
+        public string Date_of_birth
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [HiddenInput]
+        [BindProperty]
+        public string password
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [BindProperty]
+        public string description
+        {
+            get;
+            set;
+        }
+
+
+
+
+
+    }
+}
